8fcd834b4669acd29a6e18f9031cfa80345299a3
Alex.KinK <altaudiolabs@gmail.com>
2017-03-06 10:51:39 +0000

    Requested Kate Bush albums, and returned array with _.map. _.uniqBy used to removed duplicates

--- src/components/importExportTasks/index.js
+++ src/components/importExportTasks/index.js
@@ -1,10 +1,23 @@
 import React from 'react' //ignore this
 import request from 'superagent'
+import _ from 'lodash'
 
-let bushReq =request
+request
   .get('https://api.spotify.com/v1/search?q=kate%20bush&type=artist')
   	.end(function(err, res){
-  		let bushInfo = JSON.parse(bushReq.xhr.response);
-  		console.log(bushInfo);
-  		console.log(`Kate bush has ${bushInfo.artists.items[0].followers.total} followers`);
+  		console.log(res);
   	});
+
+request
+  .get('https://api.spotify.com/v1/artists/1aSxMhuvixZ8h9dK9jIDwL/albums')
+  	.end(function(err, res){
+  		let bushAlbums =
+  			_.uniqBy(
+  				(_.map(res.body.items, (obj) => {
+  			      return obj.name;
+  		        })
+  		    ), (album)=>{return album});
+
+  		console.log(bushAlbums);
+  	});
+
